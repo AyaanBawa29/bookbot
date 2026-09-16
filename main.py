840455bef45741311904f762e69c2b7bbd571090
AyaanBawa29 <29ayaan.bawa@wollemi.nsw.edu.au>
2026-09-17 09:57:20 +1000

    Second Commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+# ./books/frankenstein.txt
+
+def get_book_text(path_to_file): 
+    with open(path_to_file, encoding="utf-8") as f:
+        return f.read()
+
+def main(): 
+    booktext = get_book_text("./books/frankenstein.txt")
+    print(booktext)
+main()
