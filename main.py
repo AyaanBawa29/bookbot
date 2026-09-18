7f078dc4de627629501eed90bed6799e81da617a
AyaanBawa29 <29ayaan.bawa@wollemi.nsw.edu.au>
2026-09-18 14:28:48 +1000

    Fourth commit

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 # ./books/frankenstein.txt
+from stats import count_words, count_characters, chars_dict_to_sorted_list
 
-def get_book_text(path_to_file): 
+def get_book_text(path_to_file):
     with open(path_to_file, encoding="utf-8") as f:
         return f.read()
 
-def main(): 
+def print_report(block_path: str, word_count: int, sorted_chars_list: list[tuple[str, int]]):
+    print(f"--- begin report of {block_path} ---")
+    print(f"(word_count) words found in the document/n")
+    for char, count in sorted_chars_list: 
+        if char.isalpha():
+            print(f"the '{char}' character was found {count} times")
+    print("--- END ---")      
+
+def main():
     booktext = get_book_text("./books/frankenstein.txt")
-    print(booktext)
+    char_counts_dict = count_characters(booktext)
+    sorted_chars_list = chars_dict_to_sorted_list(char_counts_dict)
+    print_report("./books/frankenstein.txt", count_words(booktext), sorted_chars_list)
+
 main()
+
+
--- stats.py
+++ stats.py
@@ -1,5 +1,24 @@
+def count_words(book_text: str) -> int:
+    return len(book_text.split())
+
 def count_characters(book_text: str) -> dict[str, int]:
     char_counts = {}
     for char in book_text.lower():
         char_counts[char] = char_counts.get(char, 0) + 1
-    return char
+    return char_counts
+
+def sort_on(item: tuple[str, int]) -> int: 
+    return item[1]
+
+def chars_dict_to_sorted_list(chars_dict: dict[str, int]) -> list[tuple[str, int]]: 
+    my_list = []
+    for char in chars_dict.keys():
+        count = chars_dict[char]
+        my_list.append((char, count))
+    return sorted(my_list, key=sort_on, reverse=True)
+
+def list_function(): 
+    my_list = []
+    my_list.append("item")
+    return my_list 
+
